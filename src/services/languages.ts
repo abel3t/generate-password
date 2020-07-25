3035eb6c6592549f60a70cfc918d1095a6ee8a99
Abel Tran <abeltran.develop@gmail.com>
2020-07-25 15:10:50 +0700

    Add 4 Languages

--- src/services/languages.ts
+++ src/services/languages.ts
@@ -39,6 +39,58 @@ export const languagesText = {
     excludeSimilarCharacters: '似た文字を除外する:',
     excludeAmbiguousCharacters: '記号を含む:',
     password: '新しいパスワード:',
+  },
+  fr: {
+    name: 'Français',
+    title: 'Générateur de mots de passe',
+    h1: 'Générateur de mots de passe',
+    passwordLength: 'Longueur du mot de passe:',
+    includeSymbols: 'Inclure des symboles:',
+    includeNumbers: 'Inclure des numéros:',
+    includeLowercaseCharacters: 'Inclure les caractères minuscules:',
+    includeUppercaseCharacters: 'Inclure les caractères majuscules:',
+    excludeSimilarCharacters: 'Exclure les caractères similaires:',
+    excludeAmbiguousCharacters: 'Exclure les caractères ambigus:',
+    password: 'Vos nouveaux mots de passe:'
+  },
+  cn: {
+    name: '中文(台灣)',
+    title: '安全密碼生成器',
+    h1: '安全密碼生成器',
+    passwordLength: '密碼長度:',
+    includeSymbols: '包括符號:',
+    includeNumbers: '包括數字:',
+    includeLowercaseCharacters: '包括小寫字符:',
+    includeUppercaseCharacters: '包括大寫字符:',
+    excludeSimilarCharacters: '排除相似字符:',
+    excludeAmbiguousCharacters: '排除歧義字符:',
+    password: '您的新密碼:'
+  },
+  pt: {
+    name: 'Português',
+    title: 'Gerador de Senhas Seguras',
+    h1: 'Gerador de Senhas Seguras',
+    passwordLength: 'Tamanho da Senha:',
+    includeSymbols: 'Incluir Símbolos:',
+    includeNumbers: 'Incluir Números:',
+    includeLowercaseCharacters: 'Incluir Caracteres Minúsculos:',
+    includeUppercaseCharacters: 'Incluir Caracteres Maiúsculos:',
+    excludeSimilarCharacters: 'Excluir Caracteres Similares:',
+    excludeAmbiguousCharacters: 'Excluir Caracteres Ambíguos:',
+    password: 'Sua Nova Senha:',
+  },
+  kr: {
+    name: '한국어',
+    title: '보안 비밀번호 생성기',
+    h1: '보안 비밀번호 생성기',
+    passwordLength: '비밀번호 길이:',
+    includeSymbols: '기호 포함:',
+    includeNumbers: '숫자 포함:',
+    includeLowercaseCharacters: '소문자 포함:',
+    includeUppercaseCharacters: '대문자 포함:',
+    excludeSimilarCharacters: '유사한 문자 제외:',
+    excludeAmbiguousCharacters: '모호한 문자 제외:',
+    password: '새 비밀번호:',
   }
 }
 
@@ -54,6 +106,22 @@ export const languages = [
   {
     id: 'jp',
     name: '日本語'
+  },
+  {
+    id: 'fr',
+    name: 'Français'
+  },
+  {
+    id: 'cn',
+    name: '中文(台灣)'
+  },
+  {
+    id: 'pt',
+    name: 'Português'
+  },
+  {
+    id: 'kr',
+    name: '한국어'
   }
 ]
 
