d22571a1ec1997f28a19f644fa353e19e9e244b5
Abel Tran <abeltran.develop@gmail.com>
2020-07-25 14:33:23 +0700

    Add Japanese

--- src/services/languages.ts
+++ src/services/languages.ts
@@ -15,17 +15,30 @@ export const languagesText = {
   },
   vi: {
     name: 'Tiếng Việt',
-    title: 'Tạo mật khẩu',
-    h1: 'Trình tạo Mật khẩu Bảo mật',
-    passwordLength: 'Độ dài Mật khẩu:',
-    includeSymbols: 'Dùng Ký hiệu:',
+    title: 'Tạo Mật Khẩu',
+    h1: 'Trình Tạo Mật Khẩu',
+    passwordLength: 'Độ dài Mật Khẩu:',
+    includeSymbols: 'Dùng Ký Hiệu:',
     includeNumbers: 'Dùng Số:',
-    includeLowercaseCharacters: 'Dùng các Ký tự viết Thường:',
-    includeUppercaseCharacters: 'Dùng các Ký tự viết Hoa:',
-    excludeSimilarCharacters: 'Loại trừ các Ký tự Giống nhau:',
-    excludeAmbiguousCharacters: 'Loại trừ các Ký tự Nhập nhằng:',
-    password: 'Mật khẩu của bạn:',
-    buttonGenerate: 'Tạo mật khẩu'
+    includeLowercaseCharacters: 'Dùng Các Ký Tự Viết Thường:',
+    includeUppercaseCharacters: 'Dùng Các Ký Tự Viết Hoa:',
+    excludeSimilarCharacters: 'Loại Trừ Các Ký Tự Giống Nhau:',
+    excludeAmbiguousCharacters: 'Loại Trừ Các Ký Tự Nhập Nhằng:',
+    password: 'Mật Khẩu Của Bạn:',
+    buttonGenerate: 'Tạo Mật Khẩu'
+  },
+  jp: {
+    name: '日本語',
+    title: 'パスワード生成',
+    h1: 'パスワード生成',
+    passwordLength: 'パスワードの長さ:',
+    includeSymbols: '手紙で始まる:',
+    includeNumbers: '数字を含む:',
+    includeLowercaseCharacters: '小文字を含む:',
+    includeUppercaseCharacters: '大文字を含む:',
+    excludeSimilarCharacters: '似た文字を除外する:',
+    excludeAmbiguousCharacters: '記号を含む:',
+    password: '新しいパスワード:',
   }
 }
 
@@ -37,6 +50,10 @@ export const languages = [
   {
     id: 'vi',
     name: 'Tiếng Việt'
+  },
+  {
+    id: 'jp',
+    name: '日本語'
   }
 ]
 
